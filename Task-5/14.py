e514d1e96b5bb8bf878284c6407948c813dcc7ea
Almas Banu <107671834+almas-banu@users.noreply.github.com>
2022-08-02 19:57:53 +0530

    Add files via upload

---
+++ Task-5/14.py
@@ -0,0 +1,21 @@
+# Validating UID
+
+import re
+
+n = int(input())
+
+for i in range(n):
+    uid = ''.join(sorted(input()))
+    
+    try:
+        assert re.search(r'[A-Z]{2}',uid)
+        assert re.search(r'\d\d\d',uid)
+        assert not re.search(r'[^a-zA-Z0-9]',uid)
+        assert not re.search(r'(.)\1',uid)
+        assert len(uid) == 10
+        
+    except:
+        print("Invalid")
+        
+    else:
+        print("Valid")
