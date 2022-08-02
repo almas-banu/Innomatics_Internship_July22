663091d98287d06090f8924e834e61186bddbeda
Almas Banu <107671834+almas-banu@users.noreply.github.com>
2022-08-02 19:59:12 +0530

    Add files via upload

---
+++ Task-5/17.py
@@ -0,0 +1,30 @@
+# Matrix Script
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+en_string = "".join([matrix[j][i] for i in range(m) for j in range(n)])
+pattern =r'(?<=[a-zA-Z0-9])[^a-zA-Z0-9]+(?=[a-zA-Z0-9])'
+print(re.sub(pattern,' ',en_string))
